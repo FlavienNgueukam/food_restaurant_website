a11ce6a6068fd4cb6b3b9dc677a03194f26a7351
FlavienNgueukam <briatorngueukam@gmail.com>
2023-12-29 23:17:39 -0800

    component files

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:briatorngueukam@gmail.com">briatorngueukam@gmail.com</a>
+         <a href="mailto:ngueliebou.flavien@yahoo.com">ngueliebou.flavien@yahoo.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>00:07am to 00:10pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Douala, Cameroon - 8030194</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:680-845-954">680-845-954</a>
+         <a href="tel:620-179-788">620-179-788</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Flavien Briator</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
